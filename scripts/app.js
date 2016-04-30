0de767a06452c089251866083dbaf7843f89fe87
Jasmine Martin <jasminejeane@gmail.com>
2016-04-29 19:59:15 -0700

    incorporated toggle functionality and repeat

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,20 @@
 angular.module("todoListApp", [])
 
 .controller('mainCtrl', function($scope) {
-  $scope.helloWorld = function(){ 
-
-    console.log("Hello there! The is the hello World controller");
+  $scope.helloWorld = function() {
+    console.log("Hello there! This the helloWorld controller function, in the mainCtrl!");
   };
+  
+  $scope.todos = [
 
+  {"name": "Focus on angular"},
+  {"name": "You may have to cancel BGC volunteering"},
+  {"name": "Do rangular app"},
+  {"name": "Finish this tutorial"},
+  {"name": "Watch Chronicals of Narnia"},
+  {"name": "Clean up for your guest"},
+  {"name": "Stay away from negativity"}
+  
+
+]
 });
-
